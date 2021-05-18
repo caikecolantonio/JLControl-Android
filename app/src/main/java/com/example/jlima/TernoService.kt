36b3ba0ef54f798ad20c0834e0c508e299abd85f
Robson Lino <robson.leonardo.lino@hotmail.com>
2021-05-18 02:02:29 -0300

    Mudança para o servidor de produção

--- app/src/main/java/com/example/jlima/TernoService.kt
+++ app/src/main/java/com/example/jlima/TernoService.kt
@@ -10,7 +10,7 @@ import java.net.URL
 import kotlin.random.Random
 
 object TernoService {
-    val host = "http://10.0.2.2:8000"
+    val host = "https://jlima-control.herokuapp.com"
     fun getTernos (context: Context): List<ListaTerno> {
         val url = "$host/trajes/"
 
